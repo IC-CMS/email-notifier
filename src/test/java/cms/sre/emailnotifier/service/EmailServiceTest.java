fe7c2e6ba0376f3c59e5386b30af1ed07a1886b3
campbelldanieldc3 <40569039+campbelldanieldc3@users.noreply.github.com>
2018-07-11 10:25:44 -0400

    Added Test class functionality

--- src/test/java/cms/sre/emailnotifier/service/EmailServiceTest.java
+++ src/test/java/cms/sre/emailnotifier/service/EmailServiceTest.java
@@ -1,1 +1,22 @@
 
+@RunWith(Springrunner.class)
+@SpringBootTest(EmailServiceTest.class)
+public class EmailServiceTest{
+  @Autowired
+  private EmailService emailService;
+  
+  @Mock
+  MimeMessageSMTPDao mMsgSMTPDao;
+  
+  @Test
+  public void testAutowiring(){
+    Assert.assertNotNull(emailService);
+  }
+  
+  @Test
+  public void sendsEmailProperly(SendEmailRequest emailRequest){
+    Assert.assertTrue(emailService.sendEmail());
+  }
+  
+  
+}
