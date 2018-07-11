44ee0c3eb2dff0a8291551f336bd579fb33ee9a6
campbelldanieldc3 <40569039+campbelldanieldc3@users.noreply.github.com>
2018-07-11 13:32:22 -0400

    Update EmailServiceTest.java

--- src/test/java/cms/sre/emailnotifier/service/EmailServiceTest.java
+++ src/test/java/cms/sre/emailnotifier/service/EmailServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
