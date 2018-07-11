19c54b50fd5589413a8796c85bc36660481b1a75
campbelldanieldc3 <40569039+campbelldanieldc3@users.noreply.github.com>
2018-07-11 15:10:29 -0400

    Update EmailServiceTest.java

--- src/test/java/cms/sre/emailnotifier/service/EmailServiceTest.java
+++ src/test/java/cms/sre/emailnotifier/service/EmailServiceTest.java
@@ -40,7 +40,7 @@ public class EmailServiceTest{
   }
   @Test()
   public void noEmailWithBlankDn(){
-   SendEmailRequest sendEmailRequest = SendEmailRequest sendEmailRequest = new SendEmailRequest()
+   SendEmailRequest sendEmailRequest = new SendEmailRequest()
       .setSubject("I am the subject of King Email")
       .setBody("There can be only ONE Body")
       .setDn("CN=Kiin Do Va dvkiin1, OU=Whiterun, OU=Breezehome, OU=Empire, O=JarlBalgruuf, C=Tamriel");
