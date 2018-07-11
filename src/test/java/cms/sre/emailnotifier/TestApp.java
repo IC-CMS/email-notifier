e9e16dc15d3642247263e5f7300e5839b77aa331
campbelldanieldc3 <40569039+campbelldanieldc3@users.noreply.github.com>
2018-07-11 12:46:56 -0400

    Added Dependencies

--- src/test/java/cms/sre/emailnotifier/TestApp.java
+++ src/test/java/cms/sre/emailnotifier/TestApp.java
@@ -1,4 +1,9 @@
+package cms.sre.emailnotifier;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = TestApp.class)
 @RunWith(SpringRunner.class)
