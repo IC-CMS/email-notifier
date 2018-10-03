9857deb0636fd2d4502854a11848cfe99a80fd81
ermoffa <edward.moffatt@markpointech.com>
2018-10-03 18:46:11 +0000

    Changing WireMock port to dynamic to see if CircleCI Tests pass.

--- src/test/java/cms/sre/emailnotifier/controller/TestEmailController.java
+++ src/test/java/cms/sre/emailnotifier/controller/TestEmailController.java
@@ -27,9 +27,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -45,7 +43,7 @@ public class TestEmailController {
     private static JacksonTester<SendEmailRequest> jsonEmailRequest;
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().port(9095));
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
 
     private static Logger logger = LoggerFactory.getLogger(TestEmailController.class);
 
@@ -65,19 +63,19 @@ public class TestEmailController {
     TestRestTemplate testRestTemplate;
 
     @Before
-    public void setup(){
+    public void setup() {
         JacksonTester.initFields(this, new ObjectMapper());
     }
 
     @Test
-    public void testAutowiring(){
+    public void testAutowiring() {
         Assert.assertNotNull(testRestTemplate);
         Assert.assertNotNull(errorService);
         Assert.assertNotNull(emailSenderService);
     }
 
     @Test
-    public void objectToJson() throws Exception{
+    public void objectToJson() throws Exception {
         //ALWAYS Calls Email Service
         //Assume we have a JSON request to the Controller:
         String jsonRequest = "{\"dn\":\"CN=Doe John Smith jsdoe12\",\"subject\":\"sub\",\"body\":\"Body\"}";
@@ -86,8 +84,8 @@ public class TestEmailController {
         HttpPost httpPost = new HttpPost("http://localhost:" + this.port + "/sendEmailWithDN");
         StringEntity json = new StringEntity(jsonRequest);
         httpPost.setEntity(json);
-        httpPost.setHeader("Accept","application/json");
-        httpPost.setHeader("Content-Type","application/json");
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-Type", "application/json");
         CloseableHttpResponse response = httpClient.execute(httpPost);
 
         HttpEntity entity = response.getEntity();
@@ -100,15 +98,15 @@ public class TestEmailController {
     }
 
     @Test
-    public void sendEmailObject() throws Exception{
+    public void sendEmailObject() throws Exception {
 
         //ALWAYS Calls Email Service
 
         stubFor(post(urlEqualTo("/email"))
-                        .willReturn(aResponse()
-                                        .withStatus(HttpStatus.OK.value())
-                                        .withHeader("Content-Type", TEXT_PLAIN_VALUE)
-                                        .withBody("test")));
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", TEXT_PLAIN_VALUE)
+                        .withBody("test")));
 
         RestTemplate rt = new RestTemplate();
 
@@ -119,13 +117,14 @@ public class TestEmailController {
         email.setEmailAddress("dummy@dummy.com");
         email.setBody("Test Email");
 
-        Email returns = rt.postForObject(uri, email, Email.class,"");
+        Email returns = rt.postForObject(uri, email, Email.class, "");
 
         Assert.assertNotNull(returns);
 
     }
+
     @Test
-    public void sendEmailWithDnAddressTest(){
+    public void sendEmailWithDnAddressTest() {
 
         SendEmailRequest request = new SendEmailRequest()
                 .setSubject("sub")
@@ -140,8 +139,9 @@ public class TestEmailController {
         Assert.assertEquals(request.getSubject(), response.getSubject());
         Assert.assertEquals(request.getDn(), response.getDn());
     }
+
     @Test
-    public void emailServiceCallsIncorrectly(){
+    public void emailServiceCallsIncorrectly() {
         try {
             String jsonRequest = "{\"dn\":\"Doe John Smith jsdoe12\",\"subject\":\"sub\",\"body\":\"Body\"}";
             SendEmailRequest parsedEmailRequest = jsonEmailRequest.parseObject(jsonRequest);
@@ -151,20 +151,16 @@ public class TestEmailController {
             Assert.assertTrue(response.getSubject().equals("Bad Email Request"));
 
 
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @SpringBootApplication
-    public static class TestAppConfig extends App{
+    public static class TestAppConfig extends App {
 
 
     }
-
-
-
-
 
 
 }
